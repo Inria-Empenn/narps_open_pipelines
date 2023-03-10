b5cf638c398a771c15b985176c099fe675f7c967
Boris Clénet <boris.clenet@inria.fr>
2023-03-10 14:39:11 +0100

    [CI] adding cache to pip installs

--- narps_open/utils/configuration.py
+++ narps_open/utils/configuration.py
@@ -3,6 +3,8 @@
 
 """ Accessing information about teams from the analysis_pipelines_full_configuration.tsv file """
 
+# TODO : remove this comment
+
 from os.path import join
 from csv import DictReader
 from argparse import ArgumentParser
