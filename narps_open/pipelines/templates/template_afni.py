98320bf254b61bac4d7a31c47ebab6bdc29b7c27
Yaroslav Halchenko <debian@onerussian.com>
2023-07-19 10:41:43 -0400

    typo fix for beging

--- narps_open/pipelines/templates/template_afni.py
+++ narps_open/pipelines/templates/template_afni.py
@@ -5,7 +5,7 @@
 This template can be use to reproduce a pipeline using AFNI as main software.
 
 - Replace all occurences of 48CD by the actual id of the team.
-- All lines beging [INFO], are meant to help you during the reproduction, these can be removed
+- All lines starting with [INFO], are meant to help you during the reproduction, these can be removed
 eventually.
-- Also remove lines beging with [TODO], once you did what they suggested.
+- Also remove lines starting with [TODO], once you did what they suggested.
 """
--- narps_open/pipelines/templates/template_fsl.py
+++ narps_open/pipelines/templates/template_fsl.py
@@ -5,9 +5,9 @@
 This template can be use to reproduce a pipeline using FSL as main software.
 
 - Replace all occurences of 48CD by the actual id of the team.
-- All lines beging [INFO], are meant to help you during the reproduction, these can be removed
+- All lines starting with [INFO], are meant to help you during the reproduction, these can be removed
 eventually.
-- Also remove lines beging with [TODO], once you did what they suggested.
+- Also remove lines starting with [TODO], once you did what they suggested.
 """
 
 # [TODO] Only import modules you use further in te code, remove others from the import section
