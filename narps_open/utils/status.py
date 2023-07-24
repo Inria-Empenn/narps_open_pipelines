f83fee8be44204ac1fd8f0f1db229993bb6afcfb
Boris Clénet <boris.clenet@inria.fr>
2023-07-24 11:08:37 +0200

    Resolving conflicts

--- narps_open/utils/status.py
+++ narps_open/utils/status.py
@@ -7,15 +7,16 @@ from os.path import join, basename
 from json import dumps
 from argparse import ArgumentParser
 from glob import glob
+from collections import OrderedDict
 
 from requests import get
 from importlib_resources import files
 
-from narps_open.data.description import TeamDescription
+from narps_open.utils.description import TeamDescription
 from narps_open.pipelines import implemented_pipelines
 
 def get_opened_issues():
-    """ Return a list of opened issues for the NARPS Open Pipelines project """
+    """ Return a list of opened issues and pull requests for the NARPS Open Pipelines project """
     request_url = 'https://api.github.com/repos/Inria-Empenn/narps_open_pipelines/issues'
     response = get(request_url, timeout = 2)
     response.raise_for_status()
@@ -61,15 +62,22 @@ class PipelineStatusReport():
                 description.categorized_for_analysis['analysis_SW']
             self.contents[team_id]['fmriprep'] = description.preprocessing['used_fmriprep_data']
 
-            # Get issues related to the team
+            # Get issues and pull requests related to the team
             issues = {}
+            pulls = {}
             for issue in opened_issues:
+                if issue['title'] is None or issue['body'] is None:
+                    continue
                 if team_id in issue['title'] or team_id in issue['body']:
-                    issues[issue['number']] = issue['html_url']
+                    if 'pull_request' in issue: # check if issue is a pull_request
+                        pulls[issue['number']] = issue['html_url']
+                    else:
+                        issues[issue['number']] = issue['html_url']
             self.contents[team_id]['issues'] = issues
+            self.contents[team_id]['pulls'] = pulls
 
-            # Derive the satus of the pipeline
-            has_issues = len(issues) > 0
+            # Derive the status of the pipeline
+            has_issues = len(issues) + len(pulls) > 0
             is_implemeted = pipeline_class is not None
             has_file = team_id in teams_having_pipeline
 
@@ -77,11 +85,17 @@ class PipelineStatusReport():
                 raise AttributeError(f'Pipeline {team_id} refered as implemented with no file')
 
             if not is_implemeted and not has_issues and not has_file:
-                self.contents[team_id]['status'] = 'idle'
+                self.contents[team_id]['status'] = '2-idle'
             elif is_implemeted and has_file and not has_issues:
-                self.contents[team_id]['status'] = 'done'
+                self.contents[team_id]['status'] = '0-done'
             else:
-                self.contents[team_id]['status'] = 'progress'
+                self.contents[team_id]['status'] = '1-progress'
+
+        # Sort contents with the following priorities : 1-"status", 2-"softwares" and 3-"fmriprep"
+        self.contents = OrderedDict(sorted(
+            self.contents.items(),
+            key=lambda k: (k[1]['status'], k[1]['softwares'], k[1]['fmriprep'])
+            ))
 
     def markdown(self):
         """ Return a string representing the report as markdown format """
@@ -92,23 +106,23 @@ class PipelineStatusReport():
         output_markdown += '<br>:red_circle: not started yet\n'
         output_markdown += '<br>:orange_circle: in progress\n'
         output_markdown += '<br>:green_circle: completed\n'
-        output_markdown += '<br>The *softwares* column gives a simplified version of what can be '
-        output_markdown += 'found in the [team descriptions](/docs/description.md) under the '
+        output_markdown += '<br><br>The *softwares used* column gives a simplified version of '
+        output_markdown += 'what can be found in the team descriptions under the '
         output_markdown += '`general.software` column.\n'
 
         # Start table
         output_markdown += '| team_id | status | softwares used | fmriprep used ? |'
-        output_markdown += ' related issues |\n'
-        output_markdown += '| --- |:---:| --- | --- | --- |\n'
+        output_markdown += ' related issues | related pull requests |\n'
+        output_markdown += '| --- |:---:| --- | --- | --- | --- |\n'
 
         # Add table contents
         for team_key, team_values in self.contents.items():
             output_markdown += f'| {team_key} '
 
             status = ''
-            if team_values['status'] == 'done':
+            if team_values['status'] == '0-done':
                 status = ':green_circle:'
-            elif team_values['status'] == 'progress':
+            elif team_values['status'] == '1-progress':
                 status = ':orange_circle:'
             else:
                 status = ':red_circle:'
@@ -121,7 +135,14 @@ class PipelineStatusReport():
             for issue_number, issue_url in team_values['issues'].items():
                 issues += f'[{issue_number}]({issue_url}), '
 
-            output_markdown += f'| {issues} |\n'
+            output_markdown += f'| {issues} '
+
+            pulls = ''
+            for issue_number, issue_url in team_values['pulls'].items():
+                pulls += f'[{issue_number}]({issue_url}), '
+
+            output_markdown += f'| {pulls} |\n'
+
 
         return output_markdown
 
