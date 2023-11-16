202fa49407a4d8952e92f57c16711717968254e2
Boris Clénet <boris.clenet@inria.fr>
2023-11-16 17:33:58 +0100

    [ENH] add voxel_dimensions [TEST] add test for voxel_dimensions

--- tests/core/test_common.py
+++ tests/core/test_common.py
@@ -19,7 +19,7 @@ from pytest import mark, fixture
 from nipype import Node, Function
 
 from narps_open.utils.configuration import Configuration
-from narps_open.core.common import remove_files
+import narps_open.core.common as co
 
 TEMPORARY_DIR = join(Configuration()['directories']['test_runs'], 'test_common')
 
@@ -38,7 +38,7 @@ class TestCoreCommon:
     @staticmethod
     @mark.unit_test
     def test_remove_file(remove_test_dir):
-        """ Test the remove_files function with only one file """
+        """ Test the remove_file function """
 
         # Create a single file
         test_file_path = abspath(join(TEMPORARY_DIR, 'file1.txt'))
@@ -49,41 +49,13 @@ class TestCoreCommon:
 
         # Create a Nipype Node using remove_files
         test_remove_file_node = Node(Function(
-            function = remove_files,
-            input_names = ['_', 'files'],
+            function = co.remove_file,
+            input_names = ['_', 'file_name'],
             output_names = []
-            ), 'test_remove_file_node')
+            ), name = 'test_remove_file_node')
         test_remove_file_node.inputs._ = ''
-        test_remove_file_node.inputs.files = test_file_path
+        test_remove_file_node.inputs.file_name = test_file_path
         test_remove_file_node.run()
 
         # Check file is removed
         assert not exists(test_file_path)
-
-    @staticmethod
-    @mark.unit_test
-    def test_remove_files(remove_test_dir):
-        """ Test the remove_files function with a list of files """
-
-        # Create a list of files
-        test_file_paths = [
-            abspath(join(TEMPORARY_DIR, 'file1.txt')),
-            abspath(join(TEMPORARY_DIR, 'file2.txt'))
-            ]
-        for test_file_path in test_file_paths:
-            Path(test_file_path).touch()
-            assert exists(test_file_path)
-
-        # Create a Nipype Node using remove_files
-        test_remove_files_node = Node(Function(
-            function = remove_files,
-            input_names = ['_', 'files'],
-            output_names = []
-            ), 'test_remove_files_node')
-        test_remove_files_node.inputs._ = ''
-        test_remove_files_node.inputs.files = test_file_paths
-        test_remove_files_node.run()
-
-        # Check files are removed
-        for test_file_path in test_file_paths:
-            assert not exists(test_file_path)
