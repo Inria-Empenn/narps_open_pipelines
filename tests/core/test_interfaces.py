4ffaa5ca9fa06e91f7c41f2e27877e84c921a9f6
Boris Clénet <boris.clenet@inria.fr>
2024-01-26 11:57:23 +0100

    Creating an interface factory

--- tests/core/test_interfaces.py
+++ tests/core/test_interfaces.py
@@ -11,7 +11,7 @@ Usage:
     pytest -q test_interfaces.py -k <selected_test>
 """
 
-from pytest import mark
+from pytest import mark, raises
 
 from nipype.interfaces.base.core import Interface
 from nipype.interfaces.utility import Select, Function
@@ -34,8 +34,8 @@ class TestInterfaceCreator:
     def test_create_interface():
         """ Test the create_interface method """
 
-        test_node = ValidNC.create_interface()
-        assert isinstance(test_node, Select)
+        test_interface = ValidNC.create_interface()
+        assert isinstance(test_interface, Select)
 
 class TestRemoveParentDirectoryInterfaceCreator:
     """ A class that contains all the unit tests for the
@@ -47,9 +47,9 @@ class TestRemoveParentDirectoryInterfaceCreator:
     def test_create_interface():
         """ Test the create_interface method """
 
-        test_node = interfaces.RemoveParentDirectoryInterfaceCreator.create_interface()
-        assert isinstance(test_node, Function)
-        inputs = str(test_node.inputs)
+        test_interface = interfaces.RemoveParentDirectoryInterfaceCreator.create_interface()
+        assert isinstance(test_interface, Function)
+        inputs = str(test_interface.inputs)
         assert '_ = <undefined>' in inputs
         assert 'file_name = <undefined>' in inputs
         assert 'function_str = def remove_parent_directory(_, file_name: str) -> None:' in inputs
@@ -62,9 +62,9 @@ class TestRemoveDirectoryInterfaceCreator:
     def test_create_interface():
         """ Test the create_interface method """
 
-        test_node = interfaces.RemoveDirectoryInterfaceCreator.create_interface()
-        assert isinstance(test_node, Function)
-        inputs = str(test_node.inputs)
+        test_interface = interfaces.RemoveDirectoryInterfaceCreator.create_interface()
+        assert isinstance(test_interface, Function)
+        inputs = str(test_interface.inputs)
         assert '_ = <undefined>' in inputs
         assert 'directory_name = <undefined>' in inputs
         assert 'function_str = def remove_directory(_, directory_name: str) -> None:' in inputs
@@ -77,9 +77,27 @@ class TestRemoveFileInterfaceCreator:
     def test_create_interface():
         """ Test the create_interface method """
 
-        test_node = interfaces.RemoveFileInterfaceCreator.create_interface()
-        assert isinstance(test_node, Function)
-        inputs = str(test_node.inputs)
+        test_interface = interfaces.RemoveFileInterfaceCreator.create_interface()
+        assert isinstance(test_interface, Function)
+        inputs = str(test_interface.inputs)
+        assert '_ = <undefined>' in inputs
+        assert 'file_name = <undefined>' in inputs
+        assert 'function_str = def remove_file(_, file_name: str) -> None:' in inputs
+
+class TestInterfaceFactory:
+    """ A class that contains all the unit tests for the InterfaceFactory class."""
+
+    @staticmethod
+    @mark.unit_test
+    def test_create():
+        """ Test the create method """
+
+        with raises(KeyError):
+            interfaces.InterfaceFactory.create('fake_function')
+
+        test_interface = interfaces.InterfaceFactory.create('remove_file')
+        assert isinstance(test_interface, Function)
+        inputs = str(test_interface.inputs)
         assert '_ = <undefined>' in inputs
         assert 'file_name = <undefined>' in inputs
         assert 'function_str = def remove_file(_, file_name: str) -> None:' in inputs
